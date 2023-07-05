8ea6aef388c628e3a606001c08df177f7d4c2322
Harshita <starlastyle103@gmail.comm>
2023-07-06 00:02:51 +0530

    flask webframe

--- src/utils.py
+++ src/utils.py
@@ -21,8 +21,12 @@ def save_obj(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-
-
+def load_object(file_path):
+    try:
+        with open (file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+            raise CustomException(e,sys)
 
 
 
